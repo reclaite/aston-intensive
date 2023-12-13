2096b215c129ffd06cd6ef775fff14175ce64253
Alexander Gordienko <reclaite@gmail.com>
2023-12-13 23:47:32 +0300

    Spring initialization via initializr

---
+++ src/main/java/com/reclaite/AstonIntensiveApplication.java
@@ -0,0 +1,13 @@
+package com.reclaite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonIntensiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstonIntensiveApplication.class, args);
+    }
+
+}
